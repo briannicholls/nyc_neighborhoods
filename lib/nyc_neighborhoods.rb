7bf18f37f3aba15a83b7da224c4e87c508a8c0cd
Brian Nicholls <nichol88@gmail.com>
2020-02-12 06:31:31 -0500

    final working version

--- lib/nyc_neighborhoods.rb
+++ lib/nyc_neighborhoods.rb
@@ -7,7 +7,7 @@ module NycNeighborhoods
   # Returns Neighborhood name from Zip Code input
   def self.neighborhood(zip_code)
     zip_code = zip_code.to_s
-    data = JSON.parse(File.read 'data.rb')
+    data = JSON.parse(File.read 'lib/data.rb')
     data.each do |hood_hash|
       return hood_hash['neighborhood'] if hood_hash['zip_codes'].include?(zip_code)
     end
