f8c10c2bcc53adc2bd4033d752f2a2a8f9006aaf
Brian Nicholls <nichol88@gmail.com>
2020-02-12 06:29:30 -0500

    bug fix

--- lib/nyc_neighborhoods.rb
+++ lib/nyc_neighborhoods.rb
@@ -1,13 +1,13 @@
 require "nyc_neighborhoods/version"
 require 'json'
-#require_relative './nyc_neighborhoods/main.rb'
+require_relative 'data'
 module NycNeighborhoods
   class Error < StandardError; end
 
   # Returns Neighborhood name from Zip Code input
   def self.neighborhood(zip_code)
     zip_code = zip_code.to_s
-    data = JSON.parse(File.read './lib/data.json')
+    data = JSON.parse(File.read 'data.rb')
     data.each do |hood_hash|
       return hood_hash['neighborhood'] if hood_hash['zip_codes'].include?(zip_code)
     end
